5dc8e18d32601e5bcb89a9bae460f3fda150baac
meltyness <86126050+meltyness@users.noreply.github.com>
2024-12-29 23:11:56 -0600

    r

--- pawl-js/src/WelcomeLanding.js
+++ pawl-js/src/WelcomeLanding.js
@@ -3,7 +3,7 @@ import { IconTool } from '@tabler/icons-react';
 export default function WelcomeLanding() {
     return (
         <div>
-            <h1> <IconTool /> Welcome to Ratchet!  </h1>
+            <h1> <IconTool /> Welcome to ratchet!  </h1>
             <p> Check the menu to get started. ✅ </p>
         </div>
     );
